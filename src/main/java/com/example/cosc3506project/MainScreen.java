0126626db98499d5481f1afd3d31f2cb8c2a369d
Hobins <fonzieman1@hotmail.com>
2024-11-27 16:01:25 -0500

    Refactored HelloApplication

---
+++ src/main/java/com/example/cosc3506project/MainScreen.java
@@ -0,0 +1,35 @@
+package com.example.cosc3506project;
+
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+
+public class MainScreen extends Application {
+
+    private BorderPane mainLayout;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        // Create the main layout
+        mainLayout = new BorderPane();
+        // Set the title of the Stage
+        stage.setTitle("Hello Application");
+
+        // Set the initial screen content (e.g., Login screen)
+        mainLayout.setCenter(new Label("Welcome to the Main Screen!"));
+
+        // Create and display the scene
+        Scene scene = new Scene(mainLayout, 1000, 800);
+        stage.setTitle("Hello Application");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
